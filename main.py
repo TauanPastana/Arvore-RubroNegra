7454f81df36e6f14465264fd54c8f83e67bef3e7
TauanPastana <wendreotauan77@gmail.com>
2026-02-04 16:06:21 -0300

    Adicionando gitignore

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from RBTree import RBTree
 
-# Criar uma árvore rubro-negra
+
 arvore = RBTree()
 
-# Inserir alguns valores para teste
-print("Inserindo valores na árvore...")
-valores = [5,3,6,2,8,9,1,]
+
 
 
 arvore.insert(5, arvore.root)
@@ -17,7 +15,7 @@ arvore.insert(9, arvore.root)
 arvore.insert(12, arvore.root)
 
 
-# Exibir a árvore em ordem (in-order traversal)
+
 print("\nÁrvore Rubro-Negra (em ordem):")
 print("Valor\tCor")
 print("-" * 20)
